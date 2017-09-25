2c833af90cc2d748d185fee6605acdcef6ca8e15
mmoaay <mmoaay@sina.com>
2017-09-25 19:19:14 +0800

    optimize

--- util/gitflow_operator.rb
+++ util/gitflow_operator.rb
@@ -4,4 +4,10 @@ class GitflowOperator
   def create_feature(path, feature_name)
     p `cd #{path}; git flow feature start #{feature_name}`
   end
+
+  def stash(path, feature_name)
+  end
+
+  def apply_stash(path, feature_name)
+  end
 end
