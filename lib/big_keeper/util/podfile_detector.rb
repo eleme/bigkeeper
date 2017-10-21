be6f44b890cb44b2ffea9203ca1613b33b8a830a
Serena_R <rui.li07@ele.me>
2017-10-21 11:10:04 +0800

    分析未锁定pod列表

--- lib/big_keeper/util/podfile_detector.rb
+++ lib/big_keeper/util/podfile_detector.rb
@@ -1,13 +1,13 @@
-require 'tempfile'
-require 'fileutils'
 require '../util/param_parser'
-require '../util/podfile_operator'
-require '../model/podfile_type'
-
-
 
 params = BigKeeper::ParamParser.new.detect_podfile_unlock_items
 main_path = File.expand_path(params[:main_path])
+
+$modular_name_list = ["ELMKnightCollegeModule","LPDFoundationKit",
+  "LPDIntelligentDispatchModular","LPDUserRelevanceModular",
+  "LPDFeedbackModular","LPDQualityControlKit","LPDWalletModular",
+  "LPDRiderClassificationModular","LPDTransferOrderModular",
+  "LPDActivityModular","LPDPOIAddressService","LPDLogger"]
 
 class PodfileDetector
   $unlock_pod_list = []
@@ -16,21 +16,64 @@ class PodfileDetector
     podfile_lines = File.readlines("#{main_path}/Podfile")
      p 'Analyzing Podfile...' unless podfile_lines.size.zero?
       podfile_lines.select do |sentence|
-        if sentence =~(/:tag => \'[0-9]\.[0-9]\.[0-9]+\'/)
-          get_pod_name(sentence)
-        end
+      deal_podfile_line(sentence) unless sentence =~(/\'[0-9]\.[0-9]\.[0-9]+\'/)
       end
       p $unlock_pod_list
   end
 
-  def get_pod_name(sentence)
-    match_data = /\'\w*\'/.match(sentence)
-    pod_name = match_data.to_a[0].gsub(/\'/,'')
-    puts pod_name
-    $unlock_pod_list << pod_name
+  def deal_podfile_line(sentence)
+    if sentence.include?('pod ')
+      pod_model = Podfile_Modle.new(sentence)
+      if pod_model.name != nil
+          $unlock_pod_list << pod_model.name unless modular_name_list.include?(pod_name)
+      end
+      return pod_model
+    end
   end
 
-  # private get_pod_name
+  def get_pod_name(sentence)
+    # match_data = /\'\w*\'/.match(sentence)
+    # pod_name = match_data.to_a[0].delete('\'')
+    pod_model = deal_podfile_line(sentence)
+    pod_name = pod_model.name if pod_model != nil && pod_model.configurations.nil
+    puts pod_name
+    $unlock_pod_list << pod_name unless modular_name_list.include pod_name
+  end
+
+  def check_lock_version
+
+  end
+
+end
+
+class Podfile_Modle
+  attr_accessor :name,:git,:path,:configurations,:branch,:tag,:comment
+  def initialize(sentence)
+    if sentence.include?('#')
+      list = sentence.split('#')
+      @comment = list[1]
+      sentence = list[0]
+    end
+
+    sentence_slip_list = sentence.split(',')
+    return if sentence_slip_list.size.zero?
+    for piece in sentence_slip_list do
+        if /:git =>/ =~ piece
+          @git = $~.post_match
+        elsif /:path =>/ =~ piece
+          @path = $~.post_match
+        elsif /:configurations =>/ =~ piece
+          @configurations = $~.post_match
+        elsif /:branch =>/ =~ piece
+          @branch = $~.post_match
+        elsif /:tag =>/ =~ piece
+          @tag = $~.post_match
+        elsif /pod /=~ piece
+          @name = $~.post_match.delete "'\n "
+        end
+        #Debug p %Q{model name:#{@name},git:#{@git},path:#{@path},config:#{@configurations},branch:#{@branch},tag:#{@tag},comment:#{@comment}}
+    end
+  end
 end
 
 p params
