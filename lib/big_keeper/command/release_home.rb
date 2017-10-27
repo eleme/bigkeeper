9cea501a6e6f1bb2b9bc4a5d451291dd55fde58e
sfmDev <sfmdeveloper@icloud.com>
2017-10-27 15:37:04 +0800

    Merge branch 'develop' into feature/sfm_start_main_release
    * develop:
      add finish feature
      feature start optimize
    
    # Conflicts:
    #	lib/big_keeper.rb

--- lib/big_keeper/command/release_home.rb
+++ lib/big_keeper/command/release_home.rb
@@ -17,9 +17,12 @@ module BigKeeper
   end
 
   def self.release_home_finish(path, version)
-    p `git add .`
-    p `git commit -m "release: V #{version}"`
-    GitflowOperator.new.finish_release(path, version)
+    Dir.chdir(path) do
+      p `git add .`
+      p `git commit -m "release: V #{version}"`
+      p `git push origin release/#{version}`
+      GitflowOperator.new.finish_release(path, version)
+    end
   end
 
   private
