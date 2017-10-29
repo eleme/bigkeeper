c97a135754426cf36a55711fbfd79b52d55d0797
mmoaay <mmoaay@sina.com>
2017-10-29 19:10:55 +0800

    complete feature update

---
+++ lib/big_keeper/command/feature_update.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/ruby
+
+require './big_keeper/util/podfile_operator'
+require './big_keeper/util/gitflow_operator'
+require './big_keeper/util/bigkeeper_parser'
+
+require './big_keeper/model/podfile_type'
+
+require './big_keeper/service/stash_service'
+require './big_keeper/service/module_service'
+
+
+module BigKeeper
+  def self.feature_update(path, version, user, name, modules)
+    # Parse Bigkeeper file
+    BigkeeperParser.parse("#{path}/Bigkeeper")
+
+    version = BigkeeperParser.version if version == 'Version in Bigkeeper file'
+    feature_name = "#{version}_#{user}_#{name}"
+    branch_name = "#{GitflowType.name(GitflowType::FEATURE)}/#{feature_name}"
+
+    GitService.new.verify_branch(path, branch_name, OperateType::UPDATE)
+
+    current_modules = PodfileOperator.new.modules_with_type("#{path}/Podfile",
+                              BigkeeperParser.module_names, ModuleType::PATH)
+
+    # Handle modules
+    if modules
+      # Verify input modules
+      BigkeeperParser.verify_modules(modules)
+    else
+      # Get all modules if not specified
+      modules = BigkeeperParser.module_names
+    end
+
+    add_modules = modules - current_modules
+    del_modules = current_modules - modules
+
+    # Modify podfile as path and Start modules feature
+    add_modules.each do |module_name|
+      ModuleService.new.add(path, user, module_name, feature_name, GitflowType::FEATURE)
+    end
+
+    del_modules.each do |module_name|
+      ModuleService.new.del(path, user, module_name, feature_name, GitflowType::FEATURE)
+    end
+
+    # pod install
+    p `pod install --project-directory=#{path}`
+
+    # Open home workspace
+    p `open #{path}/*.xcworkspace`
+  ensure
+  end
+end
