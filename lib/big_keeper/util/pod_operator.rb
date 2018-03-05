f3df124239f6e235613522f168a78bd58f88a15e
Serena_R <lirui050716@163.com>
2018-03-05 16:52:55 +0800

    print stdout information , fix a bug about interrupt 'pod install'

--- lib/big_keeper/util/pod_operator.rb
+++ lib/big_keeper/util/pod_operator.rb
@@ -1,14 +1,26 @@
 require 'big_keeper/util/logger'
+require 'open3'
 
 module BigKeeper
   class PodOperator
     def self.pod_install(path, repo_update)
-      Logger.highlight('Start pod install, waiting...')
-
       # pod install
-      `pod repo update` if repo_update
-      `pod install --project-directory=#{path}`
-
+      if repo_update
+        Logger.highlight('Start pod repo update, waiting...')
+        cmd = 'pod repo update'
+        Open3.popen3(cmd) do |stdin , stdout , stderr, wait_thr|
+          while line = stdout.gets
+            puts line
+          end
+        end
+      end
+      Logger.highlight('Start pod install, waiting...')
+      cmd = "pod install --project-directory=#{path}"
+      Open3.popen3(cmd) do |stdin , stdout , stderr, wait_thr|
+        while line = stdout.gets
+          puts line
+        end
+      end
       Logger.highlight('Finish pod install.')
     end
   end
