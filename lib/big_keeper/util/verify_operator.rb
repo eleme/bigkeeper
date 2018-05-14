09e134afb47ef3d7e21abcff57030187cbd45bcb
mmoaay <mmoaay@sina.com>
2018-05-14 18:00:58 +0800

    release 0.8.2

--- lib/big_keeper/util/verify_operator.rb
+++ lib/big_keeper/util/verify_operator.rb
@@ -3,9 +3,9 @@ module BigKeeper
   class VerifyOperator
     def self.already_in_process?
       already_in_process = false
-      Open3.popen3('ps -ef|grep big -c') do |stdin , stdout , stderr, wait_thr|
+      Open3.popen3('ps aux | grep \<big\> -c') do |stdin , stdout , stderr, wait_thr|
         while line = stdout.gets
-          if line.rstrip.to_i > 3
+          if line.rstrip.to_i > 2
             already_in_process = true
             break
           end
