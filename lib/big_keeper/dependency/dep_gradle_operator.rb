9c36feda31c4eb3849c6f6a48d67db7e2f6360aa
潘万坤 <wankun.pan@ele.me>
2019-02-14 18:32:26 +0800

    适配Gradle

--- lib/big_keeper/dependency/dep_gradle_operator.rb
+++ lib/big_keeper/dependency/dep_gradle_operator.rb
@@ -1,50 +1,73 @@
 require 'big_keeper/dependency/dep_operator'
-require 'big_keeper/util/gradle_operator'
+
 
 module BigKeeper
   # Operator for podfile
   class DepGradleOperator < DepOperator
 
+    PATH_VERSION_CONFIG = "doc/config/version-config.gradle"
+
     def backup
-      GradleOperator.new(@path).backup
-      modules = ModuleCacheOperator.new(@path).all_path_modules
-      modules.each do |module_name|
-        module_full_path = BigkeeperParser.module_full_path(@path, @user, module_name)
-        GradleOperator.new(module_full_path).backup
-      end
+      cache_operator = CacheOperator.new(@path)
+      cache_operator.save(PATH_VERSION_CONFIG)
     end
 
     def recover
-      GradleOperator.new(@path).recover(true, false)
+      cache_operator = CacheOperator.new(@path)
+      cache_operator.load(PATH_VERSION_CONFIG)
+      cache_operator.clean
     end
 
     def update_module_config(module_name, module_operate_type)
-      module_full_path = BigkeeperParser.module_full_path(@path, @user, module_name)
-
-      # get modules
-      if ModuleOperateType::ADD == module_operate_type
-        GradleOperator.new(module_full_path).backup
-
-        add_modules = ModuleCacheOperator.new(@path).add_path_modules
-        GradleOperator.new(module_full_path).update_build_config(module_name, add_modules, ModuleOperateType::ADD)
-        GradleOperator.new(module_full_path).update_settings_config(module_name, add_modules, ModuleOperateType::ADD, @user)
-
-        del_modules = ModuleCacheOperator.new(@path).del_path_modules
-        GradleOperator.new(module_full_path).update_build_config(module_name, del_modules, ModuleOperateType::DELETE)
-        GradleOperator.new(module_full_path).update_settings_config(module_name, del_modules, ModuleOperateType::DELETE, @user)
-      elsif ModuleOperateType::DELETE == module_operate_type
-        GradleOperator.new(module_full_path).recover(true, true)
-      elsif ModuleOperateType::FINISH == module_operate_type
-        modules = ModuleCacheOperator.new(@path).all_path_modules
-        GradleOperator.new(module_full_path).update_build_config(module_name, modules, ModuleOperateType::FINISH)
-      elsif ModuleOperateType::PUBLISH == module_operate_type
-        modules = ModuleCacheOperator.new(@path).all_git_modules
-        GradleOperator.new(module_full_path).update_build_config(module_name, modules, ModuleOperateType::PUBLISH)
-        GradleOperator.new(module_full_path).recover(true, false)
+      temp_file = Tempfile.new('.version-config.gradle.tmp')
+      begin
+        File.open("#{@path}/#{PATH_VERSION_CONFIG}", 'r') do |file|
+          file.each_line do |line|
+            new_line = generate_version_config_of_line(
+              line,
+              module_name,
+              module_operate_type)
+            temp_file.puts(new_line)
+          end
+        end
+        temp_file.close
+        FileUtils.mv(temp_file.path, "#{@path}/#{PATH_VERSION_CONFIG}")
+      ensure
+        temp_file.close
+        temp_file.unlink
       end
+    end
 
-      GradleOperator.new(@path).update_build_config('', [module_name], module_operate_type)
-      GradleOperator.new(@path).update_settings_config('', [module_name], module_operate_type, @user)
+    def generate_version_config_of_line(line, module_name, module_operate_type)
+      if line.downcase.match(/([\s\S]*)#{module_name.downcase}version(\s*)=(\s*)('|")(\S*)('|")([\s\S]*)/)
+        branch_name = GitOperator.new.current_branch(@path)
+        version_name = ''
+        # Get version part of source.addition
+
+        if ModuleOperateType::ADD == module_operate_type
+          version_name = branch_name.sub(/([\s\S]*)\/([\s\S]*)/){ $2 }
+        elsif ModuleOperateType::DELETE == module_operate_type
+          return origin_config_of_module(module_name)
+        else
+          version_name = branch_name.sub(/([\s\S]*)\/(\d+.\d+.\d+)_([\s\S]*)/){ $2 }
+        end
+      return line.sub(/([\s\S]*)Version(\s*)=(\s*)('|")(\S*)('|")([\s\S]*)/){
+          "#{$1}Version = '#{version_name}'"}
+      end
+      line
+    end
+
+    def origin_config_of_module(module_name)
+      origin_config = ''
+      File.open("#{@path}/.bigkeeper/#{PATH_VERSION_CONFIG}", 'r') do |file|
+        file.each_line do |line|
+          if line.downcase.match(/([\s\S]*)#{module_name.downcase}version(\s*)=(\s*)('|")(\S*)('|")([\s\S]*)/)
+            origin_config = line
+            break
+          end
+        end
+      end
+      origin_config.chop
     end
 
     def install(should_update)
